c92790e974b3e8a04f4cabaa31edf48f58fa7754
pTSern <crackchuoi999@gmail.com>
2026-02-12 03:27:25 +0700

    * Update + add auto bat file + test window generator

--- Readme.md
+++ Readme.md
@@ -4,3 +4,21 @@
 1. Install `vcpkg` via VS 2022 in the C++ option.
 2. Setup your `VCPKG_ROOT` to env. Also add `vcpkg` to your `path`.
 3. Run `cmake --preset win-vcpkg` at `.` to install dependencies lib.
+
+# Notice
+
+- If your Clang version is higher than `11`, then goto `SDL_endian.h` file inside your `build/xx-version-installed-xx/include/SDL2/` folder, remove the `_m_prefetchw` define
+```SDL_endian.h
+    /* As of Clang 11, '_m_prefetchw' is conflicting with the winnt.h's version,
+       so we define the needed '_m_prefetch' here as a pseudo-header, until the issue is fixed. */
+    #ifdef __clang__
+    #ifndef __PRFCHWINTRIN_H
+    #define __PRFCHWINTRIN_H
+    //static __inline__ void __attribute__((__always_inline__, __nodebug__))
+    //_m_prefetch(void *__P)
+    //{
+    //  __builtin_prefetch(__P, 0, 3 /* _MM_HINT_T0 */);
+    //}
+    #endif /* __PRFCHWINTRIN_H */
+    #endif /* __clang__ */
+```
---
+++ src/main.cpp
@@ -0,0 +1,59 @@
+
+#include <SDL2/SDL.h>
+#include "SDL2/SDL_error.h"
+#include <iostream>
+#include <ostream>
+
+
+int main(int argc, char* argv[]) {
+
+    if(SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL_Init:" << SDL_GetError() << std::endl;
+    }
+    // Create Window
+    SDL_Window *win = SDL_CreateWindow("Hello pTSDL2", 100, 100, 1280, 720, SDL_WINDOW_OPENGL);
+
+    if (win == nullptr) {
+        std::cerr << "SDL Error:" << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    // Create Renderer
+    auto ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    if(ren == nullptr) {
+        SDL_DestroyWindow(win);
+        std::cerr << "SDL Renderer error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    //Create a single event loop
+    SDL_SetRenderDrawColor(ren, 40, 40, 60, 255);
+    SDL_RenderClear(ren);
+
+    bool quit = false;
+    SDL_Event event;
+
+    std::cout << "Press ESC or close the window to quit." << std::endl;
+
+    while(!quit) {
+        while (SDL_PollEvent(&event)) {
+            if(event.type == SDL_QUIT) {
+                quit = true;
+            } else if (event.type == SDL_KEYDOWN) {
+                if(event.key.keysym.sym == SDLK_ESCAPE) {
+                    quit = true;
+                }
+            }
+        }
+        SDL_RenderPresent(ren);
+    }
+
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+
+    return 0;
+}
